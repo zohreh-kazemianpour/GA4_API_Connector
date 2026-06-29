994faddeb80316814c0ffe640a7c2331313b9829
MCLIMS <bhabilsh.limbu@mcsaatchiws.com>
2026-06-29 16:39:11 +0100

    Updated to reflect the new classes, GA4ReportConfig and GA4Reporter

--- ga4_connector/__init__.py
+++ ga4_connector/__init__.py
@@ -1,3 +1,8 @@
-from .client import GA4Connector
+from .connector import GA4Connector
+from .reporter import GA4ReportConfig, GA4Reporter
 
-__all__ = ["GA4Connector"]
+__all__ = [
+    "GA4Connector",
+    "GA4ReportConfig",
+    "GA4Reporter",
+]
